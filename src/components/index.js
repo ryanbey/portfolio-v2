163a35ee368dd4fcccef6c2c71a6eda5617297ea
Ryan Bey <beycommaryan@gmail.com>
2024-08-21 21:41:50 -0600

    finished look of social buttons

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 export { default as Button } from './Button.astro'
+export { default as Bleed } from './Bleed.astro'
 export { default as Greeting } from './Greeting.astro'
 export { default as Header } from './Header.astro'
+export { default as IconButton } from './IconButton.astro'
 export { default as Navigation } from './Navigation'
 export { default as Social } from './Social.astro'
