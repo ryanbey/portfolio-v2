c5a4adf0a5734330c9543e0c8d673d0c33cc5d24
Ryan Bey <beycommaryan@gmail.com>
2024-08-31 17:27:30 -0600

    added initial experience and projects files to use to populate page later

---
+++ src/data/experience.js
@@ -0,0 +1,35 @@
+import fsLogo from '../assets/images/familysearch-logo.png'
+
+const experience = [
+  {
+    company: 'FamilySearch',
+    tenure: 'Aug 2022 - Present',
+    image: fsLogo.src,
+    positions: [
+      {
+        title: 'Web Developer II',
+        tenure: 'Jan 2023 - Present',
+        description: '',
+      },
+      {
+        title: 'Web Developer Intern',
+        tenure: 'Aug 2022 - Dec 2022',
+        description: '',
+      }
+    ],
+    skills: [
+      "React",
+      "JavaScript",
+      "SCSS",
+      "Jest",
+      "Storybook",
+      "GitHub",
+      "Jira",
+      "Heroku",
+      "Split",
+      "Figma",
+    ]
+  },
+]
+
+export default experience
---
+++ src/data/projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    title: 'Nyoom',
+    description: '',
+    image: '',
+    skills: ["React", "Next.js", "SCSS", "Cypress", "Photoshop", "XD"],
+    url: 'https://nyoom.app',
+  },
+  {
+    title: 'Just Pick Something',
+    description: '',
+    image: '',
+    skills: ["HTML", "CSS", "JavaScript", "XD"],
+    url: 'https://ryanbey.github.io/just-pick-something/',
+    githubRepoUrl: 'https://github.com/ryanbey/just-pick-something',
+  }
+]
+
+export default projects
