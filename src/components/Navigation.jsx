1810a70bf9feb2a17101d3d560494e6485b4149d
Ryan Bey <beycommaryan@gmail.com>
2024-08-31 17:13:34 -0600

    adjusted Experience component, filled home page with placeholders

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,12 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
-import '../styles/Navigation.module.scss'
+import css from'../styles/Navigation.module.scss'
 
 function Tab({ selectedTab, setSelectedTab, title, url }) {
   const style = useMemo(() => url === selectedTab ? { background: 'var(--background)', color: 'var(--text-primary) !important' } : {}, [selectedTab, title])
   const handleClick = useCallback(() => setSelectedTab(url), [setSelectedTab, title])
 
   return (
-    <a href={url} style={style} onClick={handleClick}>{title}</a>
+    <a class={css.navLink} href={url} style={style} onClick={handleClick}>{title}</a>
   )
 }
 
@@ -21,8 +21,8 @@ export default function Navigation() {
     <nav>
       <Tab title="Home" url="/" selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <Tab title="Projects" url="/projects" selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      <Tab title="Artwork" url="/artwork" selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <Tab title="Skills" url="/skills" selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <Tab title="Artwork" url="/artwork" selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <Tab title="About" url="/about" selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
     </nav>
   )
