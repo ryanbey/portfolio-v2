4b8ba2b389bb6b226c24572597fcdb8197ad7078
Ryan Bey <beycommaryan@gmail.com>
2024-09-04 11:00:29 -0600

    added some project descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,14 @@
 const projects = [
   {
     title: 'Nyoom',
-    description: '',
+    description: 'A stat tracking and comparison website for iRacing members.',
     image: '',
     skills: ["React", "Next.js", "SCSS", "Cypress", "Photoshop", "XD"],
     url: 'https://nyoom.app',
   },
   {
     title: 'Just Pick Something',
-    description: '',
+    description: 'A fun web app to help your friends and family make decisions on activites using a tournament style bracket system.',
     image: '',
     skills: ["HTML", "CSS", "JavaScript", "XD"],
     url: 'https://ryanbey.github.io/just-pick-something/',
