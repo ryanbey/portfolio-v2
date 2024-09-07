91d60322977c7ea99022bda4f889352dfbfdc3ca
Ryan Bey <beycommaryan@gmail.com>
2024-09-07 15:26:12 -0600

    added populated education section

---
+++ src/data/education.js
@@ -0,0 +1,17 @@
+import byuiLogo from '../assets/images/byui-logo.svg'
+
+const education = [
+  {
+    title: 'BYU-Idaho',
+    description: 'B.S. Software Engineering',
+    image: byuiLogo.src,
+    positions: [
+      {
+        description: 'Changed my major a couple times until I took my first web development class as an elective. I fell in love with it, changed my major to Software Engineering, and the rest is history. 3.7 GPA.'
+      },
+    ],
+    skills: ['HTML', 'CSS', 'SCSS', 'JavaScript', 'TypeScript', 'Angular', 'Node', 'Express', 'PHP', 'C++', 'Photoshop', 'XD']
+  },
+]
+
+export default education
--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,3 @@
 export { default as experience } from './experience'
 export { default as projects } from './projects'
+export { default as education } from './education'
