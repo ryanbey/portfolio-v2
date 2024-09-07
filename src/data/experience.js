5073faecdc105f72f7d766ad721686b9baf9af12
Ryan Bey <beycommaryan@gmail.com>
2024-09-06 23:03:30 -0600

    added some experience and project details

--- src/data/experience.js
+++ src/data/experience.js
@@ -9,12 +9,12 @@ const experience = [
       {
         title: 'Web Developer II',
         tenure: 'Jan 2023 - Present',
-        description: 'Building and maintaining robust React components and pages across the Memories product. Frequent contributor to our internal design system, building reusable components used across the site.Closely mentored a web developer intern including onboarding, regular guidance, and fostering independent problem - solving skills.'
+        description: 'Building and maintaining robust React components and pages across the Memories product. Frequent contributor to our internal design system building reusable components used across the site. Closely mentored a web developer intern including onboarding, regular guidance, and fostering independent problem solving skills.'
       },
       {
         title: 'Web Developer Intern',
         tenure: 'Aug 2022 - Dec 2022',
-        description: "Converted the site's international home page from Polymer to React with support for 40 languages. Discovered and improved accessibility issues across our team's product. Contributed to our internal design system.",
+        description: "Converted the site's worldwide home page from Polymer to React with support for 40 languages. Discovered and improved accessibility issues across our team's product. Contributed to our internal design system.",
       }
     ],
     skills: ["React", "JavaScript", "SCSS", "Jest", "Storybook", "GitHub", "Jira", "Heroku", "Split", "Figma"]
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,19 @@
 import nyoomLogo from '../assets/images/nyoom-logo.webp'
+import justpicksomethingLogo from '../assets/images/justpicksomething-logo.png'
 
 const projects = [
   {
     title: 'Nyoom',
     positions: [
-      { description: 'A stat tracking and comparison website for iRacing members.' }],
+      { description: 'A super fast driver stats website for iRacing members meant to offer a more performant and user-friendly alternative to the outdated iRacing profile pages on the old iRacing member site.' }],
     image: nyoomLogo.src,
     skills: ["React", "Next.js", "SCSS", "Cypress", "Photoshop", "XD"],
     url: 'https://nyoom.app',
   },
   {
     title: 'Just Pick Something',
-    positions: [{ description: 'A fun web app to help your friends and family make decisions on activites using a tournament style bracket system.' }],
-    image: nyoomLogo.src,
+    positions: [{ description: 'A fun web app to help your friends and family make decisions on activites using a tournament style bracket system. Start with a list of choices for fast food, movies, or date night ideas and see which one ends up on top.' }],
+    image: justpicksomethingLogo.src,
     skills: ["HTML", "CSS", "JavaScript", "XD"],
     url: 'https://ryanbey.github.io/just-pick-something/',
     githubRepoUrl: 'https://github.com/ryanbey/just-pick-something',
