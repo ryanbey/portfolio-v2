f0da50d2061f6a730ef0d59827664e4669e8b965
Ryan Bey <beycommaryan@gmail.com>
2024-09-12 10:29:24 -0600

    added Skills section without, counts not displayed yet

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
+export { default as allSkills } from './allSkills'
 export { default as experience } from './experience'
 export { default as projects } from './projects'
 export { default as education } from './education'
