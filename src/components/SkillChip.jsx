7eb2f0a7698706fc4e3990fafaec3b77463b0ae9
Ryan Bey <beycommaryan@gmail.com>
2024-09-12 13:48:05 -0600

    added counts support to skills chips

--- src/components/SkillChip.jsx
+++ src/components/SkillChip.jsx
@@ -1,13 +1,14 @@
 import { useCallback } from "react"
 import css from '../styles/SkillChip.module.scss'
 
-export default function SkillChip({ icon = null, text, to }) {
-  const handleClick = useCallback(() => window.open(to), [to])
+export default function SkillChip({ icon = null, text, count, to }) {
+  // const handleClick = useCallback(() => window.open(to), [to])
 
   return (
-    <button className={css.skillChip} onClick={handleClick}>
+    <button className={css.skillChip} onClick={() => console.log(`Clicked on ${text} chip!`)}>
       {icon?.src && <img src={icon.src} alt='' aria-hidden />}
       {text}
+      {count && <span className={css.skillCount}>{count}</span>}
     </button>
   )
 }
