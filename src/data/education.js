d986f74b757d7e1f088a168f0205d04d978e285b
Ryan Bey <beycommaryan@gmail.com>
2024-09-12 09:51:09 -0600

    updated med emph buttons, changed greeting buttons to non-compact

--- src/data/education.js
+++ src/data/education.js
@@ -10,7 +10,7 @@ const education = [
         description: 'Changed my major a couple times until I took my first web development class as an elective. I fell in love with it, changed my major to Software Engineering, and the rest is history. 3.7 GPA.'
       },
     ],
-    skills: ['HTML', 'CSS', 'SCSS', 'JavaScript', 'TypeScript', 'Angular', 'Node', 'Express', 'PHP', 'C++', 'Photoshop', 'XD']
+    skills: ['HTML', 'CSS', 'SCSS', 'JavaScript', 'TypeScript', 'Angular', 'Node.js', 'Express', 'PHP', 'C++', 'Photoshop', 'XD']
   },
 ]
 
