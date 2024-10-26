54ba7a1043f69006a47ca9af7db7b1d35e5a9f8f
Ryan Bey <beycommaryan@gmail.com>
2024-10-26 09:53:00 -0600

    added click event to experience containers

--- src/components/SkillChip.jsx
+++ src/components/SkillChip.jsx
@@ -2,10 +2,13 @@
 import css from '../styles/SkillChip.module.scss'
 
 export default function SkillChip({ icon = null, text, count, to }) {
-  // const handleClick = useCallback(() => window.open(to), [to])
+  const handleClick = () => {
+    console.log(`clicked on ${text} chip!`)
+    // window.open(to), [to]
+  }
 
   return (
-    <button className={css.skillChip} onClick={() => console.log(`Clicked on ${text} chip!`)}>
+    <button className={css.skillChip} onClick={handleClick}>
       {icon?.src && <img src={icon.src} alt='' aria-hidden />}
       {text}
       {count && <span className={css.skillCount}>{count}</span>}
