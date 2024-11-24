3a71f23c9ce7ee706d7dd7a793aa0813a6c004c4
Ryan Bey <beycommaryan@gmail.com>
2024-11-24 15:01:02 -0700

    gallery stlying, general styling adjustments

--- src/components/ProjectGallery.jsx
+++ src/components/ProjectGallery.jsx
@@ -3,15 +3,12 @@ import css from '../styles/ProjectGallery.module.scss'
 
 export default function ProjectGallery({ images }) {
   return (
-    <PhotoProvider
-      maskOpacity={0.5}
-      photoWrapClassName={css.photoWrap}
-      photoClassName={css.photo}
-    >
-      <div>
+    <PhotoProvider maskOpacity={0.8} className={css.galleryContainer} maskClassName={css.mask}>
+      <div className={css.galleryLayout}>
         {images?.map((item, index) => (
           <PhotoView key={index} src={item}>
-            <img src={item} alt="" height={50} width={120} style={{ objectFit: 'cover' }}/>
+            {/* Only renders first 4 images */}
+            {index < 4 && <img src={item} alt="" className={css.imageElement} draggable="false" />}
           </PhotoView>
         ))}
       </div>
