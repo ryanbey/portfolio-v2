a504bcc4464dcc2c9b2b40129c1413c7f323aea4
Ryan Bey <beycommaryan@gmail.com>
2025-03-11 16:39:33 -0600

    add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { partytown } from 'astro/config';
 
 import react from "@astrojs/react";
 
@@ -8,6 +9,10 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   adapter: netlify(),
   devToolbar: { enabled: false },
-  integrations: [react()],
+  integrations: [
+    // Used to run Google Analytics tracking in a separate thread using a web worker
+    partytown({ config: { forward: ['dataLayer.push'] } }),
+    react(),
+  ],
   trailingSlash: 'never',
 });
